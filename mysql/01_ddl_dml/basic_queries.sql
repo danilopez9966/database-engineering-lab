a84fe4e41166f31701565e4745c089834e61084a
Daniel López Arcos <150239792+danilopez9966@users.noreply.github.com>
2026-04-16 12:11:47 +0200

    Create basic_queries.sql

---
+++ mysql/01_ddl_dml/basic_queries.sql
@@ -0,0 +1,7 @@
+select * from employees;
+
+select * from employees where salary > 32000;
+
+select * from employees order by salary desc;
+
+select count(*) as 'numero de empleados' from employees;
