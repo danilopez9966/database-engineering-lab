4a1f112ec98ddd757392dfc95daaa48b131855a0
Daniel López Arcos <150239792+danilopez9966@users.noreply.github.com>
2026-04-16 12:09:02 +0200

    Create data.sql

---
+++ mysql/01_ddl_dml/data.sql
@@ -0,0 +1,5 @@
+insert into departments (name) values('HR'),('IT'),('Finance');
+insert into employees (name, position, salary, department_id) values('Dani Lopez', 'Backend Developer', 30000, 2),
+('Ana Garcia', 'HR Manager', 35000, 1),
+('Carlos Ruiz', 'Data Analyst', 32000, 3),
+('Lucia Perez', 'Frontend Developer', 28000, 2);
