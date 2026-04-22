2e597ad8abc7dfa82727fdcc5a84dbc9494afed6
Daniel López Arcos <150239792+danilopez9966@users.noreply.github.com>
2026-04-22 10:59:00 +0200

    Update procedures.sql

--- mysql/04_procedures_functions/procedures.sql
+++ mysql/04_procedures_functions/procedures.sql
@@ -29,3 +29,63 @@ begin
 end;
 delimiter;
 call get_employees_by_id(2)
+
+drop procedure if exists get_employees_by_id;
+delimiter //
+create procedure get_employees_by_id(in p_id int)
+begin
+	select id,name,position,salary
+    from employees
+    where p_id = department_id;
+end;
+delimiter;
+call get_employees_by_id(2)
+
+drop procedure if exists update_if_employee
+delimiter //
+create procedure update_if_employee (in p_id int, in p_new_salary decimal(10,2))
+begin
+	declare id_exists int;
+    
+    select count(*) into id_exists
+    from employees
+    where id = p_id;
+    
+	if p_new_salary <= 0 then
+		signal sqlstate '45000'
+		set message_text = 'no se puede insertar un salario menor o igual a 0';
+	end if;
+    
+    if id_exists = 0 then
+		signal sqlstate '45000'
+		set message_text = 'no existe el empleado';
+    end if;
+    
+    update employees
+    set salary = p_new_salary
+    where id = p_id;
+end;
+delimiter;
+call update_if_employee(3,26000);
+
+drop procedure if exists employees_in_department
+delimiter //
+create procedure employees_in_department(in p_id int)
+begin
+	declare department int;
+    
+    select count(*) into department
+    from departments
+    where id = p_id;
+    
+    if department = 0 then
+		signal sqlstate '45000'
+		set message_text = 'no existe el departamento';
+	else
+		select *
+        from employees
+        where department_id = p_id;
+	end if;
+end   
+delimiter //
+call employees_in_department(3);  
