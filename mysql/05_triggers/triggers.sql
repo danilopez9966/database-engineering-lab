0a911ac1bba8eb79650b2df1059c18e5719e0ece
Daniel López Arcos <150239792+danilopez9966@users.noreply.github.com>
2026-04-29 11:06:49 +0200

    Update triggers.sql

--- mysql/05_triggers/triggers.sql
+++ mysql/05_triggers/triggers.sql
@@ -1,1 +1,11 @@
-
+Drop trigger if exists after_update_salary_emp;
+delimiter //
+create trigger after_update_salary_emp
+after update on employees
+for each row
+begin
+	if new.salary <> old.salary then
+		insert into salary_history(employee_id,old_salary,new_salary) values(old.id,old.salary,new.salary);
+	end if;
+end //
+delimiter ;
