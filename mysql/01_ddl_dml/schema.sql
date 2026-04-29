6f5d8609380e03e9265e45335e3a68d630ef335d
Daniel López Arcos <150239792+danilopez9966@users.noreply.github.com>
2026-04-29 11:28:10 +0200

    Update schema.sql

--- mysql/01_ddl_dml/schema.sql
+++ mysql/01_ddl_dml/schema.sql
@@ -25,3 +25,16 @@ new_salary DECIMAL(10,2),
 change_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (employee_id) REFERENCES employees(id)
 );
+
+create table deleted_employees(
+id int auto_increment,
+employee_id int,
+name varchar(50),
+position varchar(50),
+salary decimal(10,2),
+department_id int,
+deleted_at datetime default CURRENT_TIMESTAMP,
+constraint pk_deleted_at primary key (id),
+constraint fk_deleted_at foreign key (employee_id) references employees (id) on update cascade on delete cascade,
+constraint fk_deletat_at_d foreign key (department_id) references departments (id) on update cascade on delete cascade
+)
